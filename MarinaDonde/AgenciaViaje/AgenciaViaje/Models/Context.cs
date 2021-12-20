7552bdf37d40bbf57a4472be07bf75b93a5a3edb
Marina Donde <madondee@hotmail.com>
2021-12-20 14:02:12 -0300

    Enviando Projeto MVC

---
+++ MarinaDonde/AgenciaViaje/AgenciaViaje/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AgenciaViaje.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Destino> Destinos { get; set; }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
---
+++ MarinaDonde/AgenciaViaje/AgenciaViaje/Models/Destino.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgenciaViaje.Models
+{
+    public class Destino
+    {
+
+        [Key]
+        public int IdDestino { get; set; }
+        
+       
+   
+        public string Cidade { get; set; }
+       
+        
+        public string Hotel { get; set; }
+
+     
+        public int Dias { get; set; }
+        
+        
+        public string Valor { get; set; }
+
+        public virtual List<Cliente> Cliente { get; set; }
+    }
+}
